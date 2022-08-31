0e4183ef4bc4fdaee2aa8426db4177f382996bdf
Marco Alejandro Egea Moreno <markoegea@gmail.com>
2022-08-30 19:31:11 -0500

    Input_function

--- kp/builtins.py
+++ kp/builtins.py
@@ -28,8 +28,20 @@ def imprimir(*args: Object) -> Object:
         print(args[0].inspect())
         return Null()
 
+def recibir(*args:Object) -> Object:
+    if len(args) != 1:
+        return Error(_WRONG_NUMBER_OF_ARGS.format(1,len(args)))
+    elif type(args[0]) == String:
+        argument = cast(String, args[0])
+        message = argument.value
+        input_data = input(message) 
+        return String(input_data)
+    else:
+        return Error(_UNSUPPORTED_ARGUMENT_TYPE.format(args[0].type().name))
+
 
 BUILTINS: Dict[str, Builtin] = {
     'longitud' : Builtin(fn=longitud),
     'imprimir' : Builtin(fn=imprimir),
+    'recibir' : Builtin(fn=recibir),
 }
